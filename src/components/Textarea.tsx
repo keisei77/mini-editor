48f3fc29828d487c4f6a6caec396a24be1a2b7ea
keisei77 <keisei.jia@qq.com>
2020-04-10 20:54:15 +0800

    :star2: add change handler

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
-const Textarea = () => {
+interface Textarea {
+  content: string;
+  changeHandler: (event: React.SyntheticEvent) => void;
+}
+
+const resetCaret = (el: HTMLElement) => {
+  const tailNode = document.createTextNode('');
+  el.appendChild(tailNode);
+
+  const isTargetFocused = document.activeElement === el;
+
+  if (tailNode !== null && tailNode.nodeValue !== null && isTargetFocused) {
+    const selection = window.getSelection();
+
+    if (selection) {
+      const range = document.createRange();
+      range.setStart(tailNode, tailNode.nodeValue.length);
+
+      range.collapse(true);
+
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+
+    if (el instanceof HTMLElement) el.focus();
+  }
+};
+
+const Textarea = (props: Textarea) => {
+  const { content, changeHandler } = props;
+  const elRef = useRef<HTMLDivElement>(null);
+
+  const isMount = !!elRef.current;
+  useEffect(() => {
+    if (!elRef.current) return;
+    if (content !== elRef.current.innerHTML) {
+      elRef.current.innerHTML = content;
+    }
+    resetCaret(elRef.current);
+  }, [content, isMount]);
+
   return (
-    <div contentEditable>
-      <div>在此输入...</div>
-    </div>
+    <div
+      contentEditable
+      ref={elRef}
+      onInput={(event) => changeHandler(event)}
+      onBlur={(event) => changeHandler(event)}
+      onKeyUp={(event) => changeHandler(event)}
+      onKeyDown={(event) => changeHandler(event)}
+      dangerouslySetInnerHTML={{ __html: content }}
+    />
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Textarea from '../components/Textarea';
 import Toolbar from '../components/Toolbar';
+import './index.scss';
 
 const Home = () => {
+  const [content, setContent] = useState<string>('<div>Hello World!</div>');
+  const changeHandler = useCallback((event: React.SyntheticEvent) => {
+    setContent(event.currentTarget.innerHTML);
+    console.log(event.target);
+  }, []);
+
   return (
-    <div>
+    <div className="home">
       <div>简易富文本编辑器</div>
       <Toolbar />
-      <Textarea />
+      <Textarea changeHandler={changeHandler} content={content} />
     </div>
   );
 };
